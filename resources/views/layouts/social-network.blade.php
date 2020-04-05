e92faa1a052b80f12788c90579b354aa7f9467c0
developers-bmotion <54631586+developers-bmotion@users.noreply.github.com>
2020-04-04 19:56:06 -0500

    subir

--- resources/views/layouts/social-network.blade.php
+++ resources/views/layouts/social-network.blade.php
@@ -3,10 +3,8 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Frogetor - Responsive Bootstrap 4 Admin Dashboard</title>
+    <title>{{ config('app.name')}} </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="A premium admin dashboard template by mannatthemes" name="description" />
-    <meta content="Mannatthemes" name="author" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- App favicon -->
     <link rel="shortcut icon" href="/assets/images/favicon.ico">
