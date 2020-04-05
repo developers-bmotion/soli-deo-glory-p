77e01cd769dd973968e91973b90c83b1e327e18f
developers-bmotion <54631586+developers-bmotion@users.noreply.github.com>
2020-04-04 19:56:59 -0500

    hola

--- resources/views/layouts/social-network.blade.php
+++ resources/views/layouts/social-network.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- App favicon -->
-    <link rel="shortcut icon" href="/assets/images/favicon.ico">
+    {{-- <link rel="shortcut icon" href="/assets/images/favicon.ico"> --}}
 
     <!-- App css -->
     @include('panels.styles')
